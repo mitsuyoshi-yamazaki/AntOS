88a2d3236d5b279e4656f68d96c96d3a5243a71d
Yamazaki Mitsuyoshi <mitsuyoshi.tsukuba@gmail.com>
2018-07-13 00:04:47 +0900

    Cache

--- src/test.ts
+++ src/test.ts
@@ -22,24 +22,32 @@ export function test(): void {
   // console.log(`${values}`)
 
   sources.forEach((source) => {
-    const amount = `${source.energy}`
-
     values[source.pos.x][source.pos.y] = source.energy
+    const value_cache: {[index: number]: number} = {}
 
     for (let x = 0; x < size; x++) {
       for (let y = 0; y < size; y++) {
         const i = source.pos.x - x
         const j = source.pos.y - y
 
-        let range = Math.max(Math.abs(i), Math.abs(j))
-        let value = source.energy
+        const range = Math.max(Math.abs(i), Math.abs(j))
+        const cached_value = value_cache[range]
 
-        while((range > 0) && (value > 1)) {
-          value = Math.ceil(value / attenuation_rate)
-          range--
+        if (cached_value) {
+          values[x][y] += cached_value
         }
+        else {
+          let value = source.energy
+          let r = range
 
-        values[x][y] += value
+          while((r > 0) && (value > 1)) {
+            value = Math.ceil(value / attenuation_rate)
+            r--
+          }
+
+          values[x][y] += value
+          value_cache[range] = value
+        }
       }
     }
   });
