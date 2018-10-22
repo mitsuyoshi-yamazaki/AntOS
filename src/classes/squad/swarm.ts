5726f6a42f412d898717668820f866410d7351c0
Yamazaki Mitsuyoshi <mitsuyoshi.tsukuba@gmail.com>
2018-10-23 00:12:04 +0900

    Update Swarm

--- src/classes/squad/swarm.ts
+++ src/classes/squad/swarm.ts
@@ -8,10 +8,15 @@ interface SwarmSquadMemory extends SquadMemory {
   max_creeps: number
   no_spawn: boolean
   messages: string[]
+  stop_by: number     // Flag no_spawn when spawned reaches stop_by
+  spawned: number     // Increase by addCreep(). Reset by no_spawn
+  debug: boolean
 }
 
 export class SwarmSquad extends Squad {
-
+  private next_creep: CreepType | null
+  private attackers: Creep[] = []
+  private healers: Creep[] = []
 
   constructor(readonly name: string, readonly base_room: Room) {
     super(name)
@@ -19,9 +24,11 @@ export class SwarmSquad extends Squad {
     this.creeps.forEach((creep) => {
       switch (creep.memory.type) {
         case CreepType.ATTACKER:
+          this.attackers.push(creep)
           break
 
         case CreepType.HEALER:
+          this.healers.push(creep)
           break
 
         default:
@@ -29,6 +36,41 @@ export class SwarmSquad extends Squad {
           break
       }
     })
+
+    this.next_creep = this.setNextCreep()
+  }
+
+  private setNextCreep(): CreepType | null {
+    const squad_memory = Memory.squads[this.name] as SwarmSquadMemory
+    if (!squad_memory) {
+      return null
+    }
+    if (squad_memory.no_spawn) {
+      squad_memory.spawned = 0
+      return null
+    }
+    if (squad_memory.stop_by <= squad_memory.spawned) {
+      squad_memory.no_spawn = true
+
+      const message = `SwarmSquad reached maximum creep size: stopped spawning ${this.name}, ${this.base_room.name}`
+      console.log(message)
+      Game.notify(message)
+
+      return null
+    }
+
+    if (this.healers.length < 2) {
+      return CreepType.HEALER
+    }
+
+    if (this.creeps.size >= squad_memory.max_creeps) {
+      return null
+    }
+
+    if (this.healers.length < this.attackers.length) {
+      return CreepType.HEALER
+    }
+    return CreepType.ATTACKER
   }
 
   public get type(): SquadType {
@@ -45,7 +87,10 @@ export class SwarmSquad extends Squad {
 
   // --
   public get spawnPriority(): SpawnPriority {
-    return SpawnPriority.NONE // @todo:
+    if (!this.next_creep) {
+      return SpawnPriority.NONE
+    }
+    return SpawnPriority.LOW
   }
 
   public hasEnoughEnergy(energyAvailable: number, capacity: number): boolean {
@@ -53,7 +98,63 @@ export class SwarmSquad extends Squad {
   }
 
   public addCreep(energyAvailable: number, spawnFunc: SpawnFunction): void {
-     // @todo:
+    const squad_memory = Memory.squads[this.name] as SwarmSquadMemory
+    if (!squad_memory) {
+      return
+    }
+
+    squad_memory.spawned += 1
+
+    switch (this.next_creep) {
+      case CreepType.ATTACKER:
+        this.addAttacker(energyAvailable, spawnFunc, squad_memory)
+        break
+
+      case CreepType.HEALER:
+        this.addHealer(energyAvailable, spawnFunc, squad_memory)
+        break
+
+      default:
+        break
+    }
+  }
+
+  private addAttacker(energyAvailable: number, spawnFunc: SpawnFunction, squad_memory: SwarmSquadMemory): void {
+    let body: BodyPartConstant[]
+
+    if (squad_memory.debug) {
+      body = [
+        ATTACK, ATTACK,
+        MOVE, MOVE,
+      ]
+    }
+    else {
+      body = [
+        ATTACK, ATTACK,
+        MOVE, MOVE,
+      ]
+    }
+
+    this.addGeneralCreep(spawnFunc, body, CreepType.ATTACKER)
+  }
+
+  private addHealer(energyAvailable: number, spawnFunc: SpawnFunction, squad_memory: SwarmSquadMemory): void {
+    let body: BodyPartConstant[]
+
+    if (squad_memory.debug) {
+      body = [
+        MOVE, MOVE,
+        HEAL, HEAL,
+      ]
+    }
+    else {
+      body = [
+        MOVE, MOVE,
+        HEAL, HEAL,
+      ]
+    }
+
+    this.addGeneralCreep(spawnFunc, body, CreepType.ATTACKER)
   }
 
   public run(): void {
