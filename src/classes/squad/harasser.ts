b1372eb8beaaa3cd6f777315a4f64b10cdece9ca
Mitsuyoshi Yamazaki <mitsuyoshi.yamazaki@speee.jp>
2018-11-09 19:45:37 +0900

    Rotate harasser targets

--- src/classes/squad/harasser.ts
+++ src/classes/squad/harasser.ts
@@ -4,6 +4,7 @@ import { CreepStatus, ActionResult, CreepType } from "classes/creep"
 
 interface HarasserSquadMemory extends SquadMemory {
   target_rooms: string[]
+  target_room_index: number
   target_ids: {[room_name: string]: string[]}
   max_creeps?: number
   no_spawn: boolean     // instantiate InvaderSquad but no spawn
@@ -11,16 +12,72 @@ interface HarasserSquadMemory extends SquadMemory {
 
 export class HarasserSquad extends Squad {
   private next_creep: CreepType | null
+  private target_room_name: string
+  private target_room: Room | undefined
 
   constructor(readonly name: string, readonly base_room: Room) {
     super(name)
+
+    const squad_memory = Memory.squads[this.name] as HarasserSquadMemory
+
+    if (squad_memory && squad_memory.target_rooms && squad_memory.target_rooms.length) {
+      const specified_target_room_name = squad_memory.target_rooms[squad_memory.target_room_index]
+      const patrolling_room_names: string[] = Array.from(this.creeps.values()).map(creep=>creep.room.name)
+
+      if (specified_target_room_name) {
+        const is_in_room = (patrolling_room_names.indexOf(specified_target_room_name) >= 0)
+
+        if (is_in_room) {
+          const specified_target_room = Game.rooms[specified_target_room_name]
+
+          if (specified_target_room) {
+            if (specified_target_room.attacker_info().hostile_creeps.length) {
+              this.target_room = specified_target_room
+              this.target_room_name = specified_target_room_name
+            }
+            else {
+              if (squad_memory.target_rooms.length) {
+                squad_memory.target_room_index = (squad_memory.target_room_index + 1) % squad_memory.target_rooms.length
+              }
+              else {
+                squad_memory.target_room_index = 0
+              }
+
+              this.target_room_name = this.base_room.name
+              this.target_room = this.base_room
+            }
+          }
+          else {
+            const message = `HarasserSquad something wrong ${this.name}`
+            console.log(message)
+            Game.notify(message)
+
+            this.target_room_name = this.base_room.name
+            this.target_room = this.base_room
+          }
+        }
+        else {
+          this.target_room = Game.rooms[specified_target_room_name]
+          this.target_room_name = specified_target_room_name
+        }
+      }
+      else {
+        squad_memory.target_room_index = 0
+        this.target_room_name = this.base_room.name
+        this.target_room = this.base_room
+        }
+    }
+    else {
+      this.target_room_name = this.base_room.name
+      this.target_room = this.base_room
+    }
 
     this.next_creep = this.setNextCreep()
   }
 
   private setNextCreep(): CreepType | null {
     const squad_memory = Memory.squads[this.name] as HarasserSquadMemory
-    if (!squad_memory) {  // @fixme: no_spawn
+    if (!squad_memory || !squad_memory.target_rooms || (squad_memory.target_rooms.length == 0)) {  // @fixme: no_spawn
       return null
     }
 
@@ -70,15 +127,12 @@ export class HarasserSquad extends Squad {
   }
 
   public run(): void {
-    const squad_memory = Memory.squads[this.name] as HarasserSquadMemory
-    const target_room = (squad_memory && squad_memory.target_rooms && squad_memory.target_rooms[0]) ? squad_memory.target_rooms[0] : this.base_room.name
-
     this.creeps.forEach(creep => {
       if (creep.spawning) {
         return
       }
 
-      creep.searchAndDestroyTo(target_room, true)
+      creep.searchAndDestroyTo(this.target_room_name, true)
     })
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -541,8 +541,8 @@ function sellOrders(resource_type: ResourceConstant, price: number): Order[] {
 /**
  * @todo:
  * auto mineral harvester
+ * general ranged attacker
  * W51S29, W58S4 -> E16N37: dismantle wall, reserve, claim multiple rooms, send resources
- * rotate harasser target rooms
  * 500k tower minimum storage fill if rcl < 8
  * minimum rampart hits
  * balancer
