7a93002e41cb678e542fadf981e45820867eddb5
Yamazaki Mitsuyoshi <mitsuyoshi.tsukuba@gmail.com>
2018-12-04 23:26:22 +0900

    Update

--- src/classes/extensions.ts
+++ src/classes/extensions.ts
@@ -1436,6 +1436,19 @@ export function tick(): void {
       if (show_logs) {
         console.log(`Error no buy order for ${resource_to_sell}`)
       }
+
+      const ally_room_name = 'W52S25'
+      const result = terminal.send(resource_to_sell, 10000, ally_room_name)
+
+      if (show_logs) {
+        if (result == OK) {
+          console.log(`Send ${resource_to_sell} to ${ally_room_name}`)
+        }
+        else {
+          console.log(`Error sending ${resource_to_sell} to ${ally_room_name} failed with ${result}`)
+        }
+      }
+
       return
     }
 
