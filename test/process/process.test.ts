df6028cf525184e277389e9fad906b6664063d27
Yamazaki Mitsuyoshi <mitsuyoshi.tsukuba@gmail.com>
2021-06-21 13:03:58 +0900

    Processのテストを追加

---
+++ test/process/process.test.ts
@@ -0,0 +1,26 @@
+import { isProcedural, isStatefulProcess, Procedural, StatefulProcess } from "../../src/process/process"
+
+class SomeObject { }
+
+test("isStatefulProcess()", () => {
+  class SomeStatefulProcess implements StatefulProcess {
+    public readonly shouldStore = true
+    public readonly processId = 0
+
+    public encode(): unknown {
+      return null
+    }
+  }
+
+  expect(isStatefulProcess(new SomeStatefulProcess())).toBe(true)
+  expect(isStatefulProcess(new SomeObject())).toBe(false)
+})
+
+test("isProcedural()", () => {
+  class SomeProceduralObject implements Procedural {
+    public runOnTick(): void { }
+  }
+
+  expect(isProcedural(new SomeProceduralObject())).toBe(true)
+  expect(isProcedural(new SomeObject())).toBe(false)
+})
