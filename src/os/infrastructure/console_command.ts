e55693063b50c2202e2c5c7585760c2a722e0bc0
Yamazaki Mitsuyoshi <mitsuyoshi.tsukuba@gmail.com>
2021-06-21 15:42:25 +0900

    helpコマンドを実装

--- src/os/infrastructure/console_command.ts
+++ src/os/infrastructure/console_command.ts
@@ -9,6 +9,7 @@ export const isConsoleCommand = (obj: string): obj is ConsoleCommandType => {
 
 interface ConsoleCommandDefinition {
   command: ConsoleCommandType
+  description: string
   options: string[]
   args: string | null
 }
@@ -22,9 +23,10 @@ export interface ConsoleCommand {
   run(): CommandExecutionResult
 }
 
-const messageDefinitions: ConsoleCommandDefinition[] = [
+const commandDefinitions: ConsoleCommandDefinition[] = [
   {
     command: "help",
+    description: "List available commands.",
     options: [],
     args: null,
   }
@@ -37,6 +39,22 @@ export class HelpCommand implements ConsoleCommand {
   public constructor() {}
 
   public run(): CommandExecutionResult {
-    return "You just called help command!" // TODO:
+    return commandDefinitions.reduce((result, current) => {
+      const options = (): string => {
+        if (current.options.length <= 0) {
+          return ""
+        }
+        return current.options.reduce((optionResult, currentOption) => {
+          return `${optionResult}\n  - ${currentOption}`
+        }, "\n- options:")
+      }
+      const args = (): string => {
+        if (current.args == null) {
+          return ""
+        }
+        return `\n- arguments: ${current.args}`
+      }
+      return `${result}\n\n# ${current.command}\n${current.description}${options()}${args()}`
+    }, "Available commands:")
   }
 }
