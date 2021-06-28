d7071608417295722dc8337f74c8a805d831c83a
Yamazaki Mitsuyoshi <mitsuyoshi.tsukuba@gmail.com>
2021-06-29 02:59:25 +0900

    Sourceまでの経路を計算

--- src/objective/room_keeper/owned_room_object_cache.ts
+++ src/objective/room_keeper/owned_room_object_cache.ts
@@ -1,5 +1,6 @@
 import { RoomName, RoomPathMemory } from "prototype/room"
 import { EnergyChargeableStructure } from "prototype/room_object"
+import { calculateSourceRoute } from "script/pathfinder"
 
 interface OwnedRoomObjects {
   controller: StructureController
@@ -110,6 +111,15 @@ function enumerateObjectsIn(room: Room): OwnedRoomObjects | null {
     }
   })
 
+  if (spawns[0] != null) {
+    calculateSourceRouteIn(room, sources, spawns[0].pos)
+  } else {
+    const spawnConstructionSite = constructionSites.find(site => site.structureType === STRUCTURE_SPAWN)
+    if (spawnConstructionSite != null) {
+      calculateSourceRouteIn(room, sources, spawnConstructionSite.pos)
+    }
+  }
+
   return {
     controller,
     sources,
@@ -131,40 +141,38 @@ function enumerateObjectsIn(room: Room): OwnedRoomObjects | null {
   }
 }
 
+function calculateSourceRouteIn(room: Room, sources: Source[], destination: RoomPosition): void {
+  const pathInMemory: RoomPathMemory = room.memory.p ?? { s: {} }
+  if (room.memory.p == null) {
+    room.memory.p = pathInMemory
+  }
 
-
-// function calculateSourceRoute(room: Room, sources: Source[], destination: RoomPosition): void {
-//   const pathInMemory: RoomPathMemory = room.memory.p ?? { s: {} }
-//   if (room.memory.p == null) {
-//     room.memory.p = pathInMemory
-//   }
-
-//   sources.forEach(source => {
-//     const sourcePath = pathInMemory.s[source.id]
-//     if (sourcePath == null) {
-//       const result = calculateSourceRoute(source.id, spawn.pos)
-//       switch (result.resultType) {
-//       case "succeeded": {
-//         const path = result.value.path.path.map(position => ({ x: position.x, y: position.y }))
-//         path.push(...result.value.harvestPositions.map(position => ({ x: position.x, y: position.y })))
-//         pathInMemory.s[source.id] = {
-//           p: path,
-//           d: { x: spawn.pos.x, y: spawn.pos.y },
-//         }
-//         console.log(`source path calculated with ${result.value.harvestPositions.length} harvest position`)
-//         break
-//       }
-//       case "failed":
-//         pathInMemory.s[source.id] = "no path"
-//         console.log(`source path cannot found: ${result.reason}`)
-//         break
-//       }
-//       return
-//     }
-//     if (sourcePath === "no path") {
-//       return
-//     }
-//     // console.log(`source path found for ${source.id}`)
-//     // sourcePath.p.forEach(position => source.room.visual.text("*", position.x, position.y))
-//   })
-// }
+  sources.forEach(source => {
+    const sourcePath = pathInMemory.s[source.id]
+    if (sourcePath === "no path") {
+      return
+    }
+    if (sourcePath != null) {
+      return
+    }
+    const result = calculateSourceRoute(source.id, destination)
+    switch (result.resultType) {
+    case "succeeded": {
+      const path = result.value.path.path
+        .map(position => ({ x: position.x, y: position.y }))
+        .splice(3, result.value.path.path.length - 3)
+      path.push(...result.value.harvestPositions.map(position => ({ x: position.x, y: position.y })))
+      pathInMemory.s[source.id] = {
+        p: path,
+        d: { x: destination.x, y: destination.y },
+      }
+      console.log(`source path calculated with ${result.value.harvestPositions.length} harvest position`)
+      break
+    }
+    case "failed":
+      pathInMemory.s[source.id] = "no path"
+      console.log(`source path cannot found: ${result.reason}`)
+      break
+    }
+  })
+}
