b3cbbb86ea8c3ef4420d5b33739d68b279d7a88a
Yamazaki Mitsuyoshi <mitsuyoshi.tsukuba@gmail.com>
2021-07-14 11:25:54 +0900

    ゲーム状態の問題とプログラミングの問題を分離

--- src/os/infrastructure/primitive_logger.ts
+++ src/os/infrastructure/primitive_logger.ts
@@ -4,12 +4,20 @@ import { coloredText } from "utility/log"
  * Processより低レベルでエラーが発生した際に使用する
  */
 export const PrimitiveLogger = {
-  log: (message: string): void => {
+  log(message: string): void {
     console.log(message)
   },
 
-  fatal: (message: string): void => {
-    const coloredMessage = coloredText(message, "critical")
+  /** ゲームの危機状態の通知 */
+  fatal(message: string): void {
+    const coloredMessage = coloredText(message, "error")
+    console.log(coloredMessage)
+    Game.notify(coloredMessage)
+  },
+
+  /** プログラムの問題の通知 */
+  programError(message: string): void {
+    const coloredMessage = coloredText(`[Program bug]: ${message}`, "critical")
     console.log(coloredMessage)
     Game.notify(coloredMessage)
   },
