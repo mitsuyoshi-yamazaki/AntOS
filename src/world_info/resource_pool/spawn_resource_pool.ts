26d3da6e9230ea5f4f53a9c53f3a64baef27fa17
Yamazaki Mitsuyoshi <mitsuyoshi.tsukuba@gmail.com>
2021-07-18 21:12:33 +0900

    Capacity以上のCreepをSpwanしようとした場合に警告を出す

--- src/world_info/resource_pool/spawn_resource_pool.ts
+++ src/world_info/resource_pool/spawn_resource_pool.ts
@@ -1,5 +1,6 @@
 import { PrimitiveLogger } from "os/infrastructure/primitive_logger"
 import { V5CreepMemory } from "prototype/creep"
+import { bodyCost } from "utility/creep_body"
 import { roomLink } from "utility/log"
 import { RoomName } from "utility/room_name"
 import { generateUniqueId } from "utility/unique_id"
@@ -31,6 +32,11 @@ export class SpawnPool implements ResourcePoolType<StructureSpawn> {
       }
       const creepName = generateUniqueId(request.codename)
       const body = request.body ?? createBodyFrom(request.roles, spawn.room.energyCapacityAvailable)
+      const cost = bodyCost(body)
+      if (cost > spawn.room.energyCapacityAvailable) {
+        PrimitiveLogger.programError(`Spawn request ${request.taskIdentifier}, ${request.roles} body is too large (${body.length}parts ${cost}Energy) in ${roomLink(this.parentRoomName)}`)
+        return
+      }
       const memory: V5CreepMemory = {
         v: "v5",
         p: request.parentRoomName ?? this.parentRoomName,
@@ -48,7 +54,7 @@ export class SpawnPool implements ResourcePoolType<StructureSpawn> {
       case ERR_NOT_ENOUGH_ENERGY:
         break
       default:
-        PrimitiveLogger.fatal(`${spawn.name} in ${roomLink(this.parentRoomName)} faild to spawn ${result}, task: ${request.taskIdentifier}, creep name: ${creepName}, body(length: ${body.length}): ${body}`)
+        PrimitiveLogger.programError(`${spawn.name} in ${roomLink(this.parentRoomName)} faild to spawn ${result}, task: ${request.taskIdentifier}, creep name: ${creepName}, body(length: ${body.length}): ${body}`)
       }
     })
   }
