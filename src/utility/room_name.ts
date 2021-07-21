cff03281f56edd8471a5a9f08c8abaf530352986
Yamazaki Mitsuyoshi <mitsuyoshi.tsukuba@gmail.com>
2021-07-21 11:01:25 +0900

    RoomCoordinateをxyで表現するよう変更

--- src/utility/room_name.ts
+++ src/utility/room_name.ts
@@ -7,7 +7,7 @@ type RoomCoordinateDirection = typeof RoomCoordinateDirection[number]
 
 export const isRoomName = (obj: string): obj is RoomName => {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  return parseRoomCoordinate(obj) != null
+  return RoomCoordinate.parse(obj) != null
 }
 
 export const isRoomCoordinateDirection = (obj: string): obj is RoomCoordinateDirection => {
@@ -15,44 +15,59 @@ export const isRoomCoordinateDirection = (obj: string): obj is RoomCoordinateDir
   return RoomCoordinateDirection.includes(obj as any)
 }
 
-export interface RoomCoordinate {
-  direction: RoomCoordinateDirection
-  latitude: number
-  longitude: number
-}
-
-export function parseRoomCoordinate(roomName: RoomName): RoomCoordinate | null {
-  const parsed = /^([WE])([0-9]+)([NS])([0-9]+)$/.exec(roomName)
-  if (parsed == null || (parsed.length < 5)) {
-    return null
+export class RoomCoordinate {
+  public get roomType(): RoomType {
+    return roomTypeFromCoordinate(this)
   }
 
-  const horizontalDirection = parsed[1]
-  const longitude = parseInt(parsed[2], 10)
-  const verticalDirection = parsed[3] as string
-  const latitude = parseInt(parsed[4], 10)
-  const direction = verticalDirection + horizontalDirection
-  if (!isRoomCoordinateDirection(direction)) {
-    return null
+  private constructor(
+    public readonly roomName: RoomName,
+    public readonly direction: RoomCoordinateDirection,
+    public readonly x: number,
+    public readonly y: number,
+  ) {
   }
 
-  return {
-    direction,
-    latitude,
-    longitude,
+  // TODO: directionを超える場合を計算する
+  public static create(direction: RoomCoordinateDirection, x: number, y: number): RoomCoordinate {
+    const normalizedX = Math.max(x, 0)
+    const normalizedY = Math.max(y, 0)
+    const roomName = `${direction[1]}${normalizedX}${direction[0]}${normalizedY}`
+    return new RoomCoordinate(roomName, direction, normalizedX, normalizedY)
+  }
+
+  public static parse(roomName: RoomName): RoomCoordinate | null {
+    const parsed = /^([WE])([0-9]+)([NS])([0-9]+)$/.exec(roomName)
+    if (parsed == null || (parsed.length < 5)) {
+      return null
+    }
+
+    const horizontalDirection = parsed[1]
+    const x = parseInt(parsed[2], 10)
+    const verticalDirection = parsed[3] as string
+    const y = parseInt(parsed[4], 10)
+    const direction = verticalDirection + horizontalDirection
+    if (!isRoomCoordinateDirection(direction)) {
+      return null
+    }
+
+    return new RoomCoordinate(roomName, direction, x, y)
   }
 }
 
 export function roomTypeOf(roomName: RoomName): RoomType | null {
-  const coordinate = parseRoomCoordinate(roomName)
+  const coordinate = RoomCoordinate.parse(roomName)
   if (coordinate == null) {
     return null
   }
+  return roomTypeFromCoordinate(coordinate)
+}
 
-  const localLatitude = coordinate.latitude % 10
-  const localLongitude = coordinate.longitude % 10
-  const verticalHighway = localLongitude === 0
-  const horizontalHighway = localLatitude === 0
+function roomTypeFromCoordinate(roomCoordinate: RoomCoordinate): RoomType {
+  const localX = roomCoordinate.x % 10
+  const localY = roomCoordinate.y % 10
+  const verticalHighway = localX === 0
+  const horizontalHighway = localY === 0
   if (verticalHighway && horizontalHighway) {
     return "highway_crossing"
   }
@@ -60,11 +75,11 @@ export function roomTypeOf(roomName: RoomName): RoomType | null {
     return "highway"
   }
 
-  if (coordinate.latitude % 5 === 0 && coordinate.longitude % 5 === 0) {
+  if (roomCoordinate.x % 5 === 0 && roomCoordinate.y % 5 === 0) {
     return "sector_center"
   }
 
-  if (localLatitude >= 4 && localLatitude <= 6 && localLongitude >= 4 && localLongitude <= 6) {
+  if (localX >= 4 && localX <= 6 && localY >= 4 && localY <= 6) {
     return "source_keeper"
   }
   return "normal"
