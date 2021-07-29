f958ad1afa4806f05d5fa7f50646b619c93d8d8b
Yamazaki Mitsuyoshi <mitsuyoshi.tsukuba@gmail.com>
2021-07-30 00:06:22 +0900

    新RoomのRemote Room情報を設定

--- src/process/onetime/season_487837_attack_invader_core_room_names.ts
+++ src/process/onetime/season_487837_attack_invader_core_room_names.ts
@@ -7,5 +7,6 @@ export const remoteRoomNamesToDefend = new Map<RoomName, RoomName[]>([
   ["W9S24", ["W9S25", "W8S24"]],
   ["W1S25", ["W2S25", "W1S24"]],
   // ["E5S23", ["E4S23", "E6S23"]],
-  ["W3S24", ["W3S23"]]
+  ["W3S24", ["W3S23"]],
+  ["W21S23", ["W21S22", "W22S23", "W21S24"]],
 ])
