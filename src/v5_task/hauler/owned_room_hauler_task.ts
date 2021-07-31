7b358c5635a526e49c3512a29b64eab01196c10d
Yamazaki Mitsuyoshi <mitsuyoshi.tsukuba@gmail.com>
2021-08-01 00:15:12 +0900

    Single Source RoomでHaulerが足りない問題を修正

--- src/v5_task/hauler/owned_room_hauler_task.ts
+++ src/v5_task/hauler/owned_room_hauler_task.ts
@@ -79,7 +79,8 @@ export class OwnedRoomHaulerTask extends Task {
   private runHauler(objects: OwnedRoomObjects, energySources: EnergySource[]): ProblemFinder[] {
     const necessaryRoles: CreepRole[] = [CreepRole.Hauler, CreepRole.Mover, CreepRole.EnergyStore]
     const filterTaskIdentifier = this.taskIdentifier
-    const minimumCreepCount = energySources.length * 1 // TODO: 距離等を加味する
+    const energyCapacity = objects.sources.reduce((result, current) => result + current.energyCapacity, 0)
+    const minimumCreepCount = Math.ceil(energyCapacity / 3000)
     const creepPoolFilter: CreepPoolFilter = creep => hasNecessaryRoles(creep, necessaryRoles)
 
     const problemFinders: ProblemFinder[] = [
