f2d92dfed3826172e48e725d28619eacde79aea8
Yamazaki Mitsuyoshi <mitsuyoshi.tsukuba@gmail.com>
2021-08-01 00:39:18 +0900

    不要なSign処理を停止

--- src/utility/system_info.ts
+++ src/utility/system_info.ts
@@ -19,7 +19,7 @@ export const SystemInfo = {
     name: "AntOS",
   },
   application: {
-    version: "6.3.0",
+    version: "6.3.1",
     shortVersionString: ShortVersion.v6,
     name: "ProblemSolver",
   },
