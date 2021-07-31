d5fae8889a0c27477f12fdf64daa706775b0b93e
Yamazaki Mitsuyoshi <mitsuyoshi.tsukuba@gmail.com>
2021-07-31 12:25:35 +0900

    一時処理

--- src/process/test/test_process.ts
+++ src/process/test/test_process.ts
@@ -1,3 +1,4 @@
+import { Sign } from "game/sign"
 import { Procedural } from "process/procedural"
 import { Process, ProcessId } from "process/process"
 import { processLog } from "process/process_log"
@@ -36,6 +37,14 @@ export class TestProcess implements Process, Procedural {
   }
 
   public runOnTick(): void {
-    processLog(this, `Test log at ${Game.time}, ${this.testMemory}`)
+    // const creep = Game.creeps["baked_tart_1346405c"]
+    // if (creep != null && creep.room.controller != null && (creep.room.controller.sign == null || creep.room.controller.sign.username !== Game.user.name)) {
+    //   creep.v5task = null
+    //   if (creep.signController(creep.room.controller, Sign.signForOwnedRoom()) === ERR_NOT_IN_RANGE) {
+    //     creep.moveTo(creep.room.controller)
+    //   }
+    // }
+
+    processLog(this, `Test log at ${Math.floor(Game.time / 20) * 20}, ${this.testMemory}`)
   }
 }
