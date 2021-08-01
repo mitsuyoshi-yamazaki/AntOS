98e1c0d8a6bdf85d4cc3a7af063fec1364c29492
Yamazaki Mitsuyoshi <mitsuyoshi.tsukuba@gmail.com>
2021-08-01 18:45:03 +0900

    Remote Room情報を更新

--- src/utility/system_info.ts
+++ src/utility/system_info.ts
@@ -19,7 +19,7 @@ export const SystemInfo = {
     name: "AntOS",
   },
   application: {
-    version: "6.4.18",
+    version: "6.4.19",
     shortVersionString: ShortVersion.v6,
     name: "DecisionMaker",
   },
