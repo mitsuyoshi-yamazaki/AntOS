7de024a7c616ac815cafa2f7312b908c21d405f2
Yamazaki Mitsuyoshi <mitsuyoshi.tsukuba@gmail.com>
2021-08-02 15:16:04 +0900

    Spawn時エラーを修正

--- src/world_info/resource_pool/spawn_resource_pool.ts
+++ src/world_info/resource_pool/spawn_resource_pool.ts
@@ -19,7 +19,7 @@ export class SpawnPool implements ResourcePoolType<StructureSpawn> {
   }
 
   public spawnCreeps(rawRequests: CreepSpawnRequest[]): void {
-    const idleSpawns = this.spawns.filter(spawn => spawn.spawning == null || spawn.spawning.remainingTime <= 0)
+    const idleSpawns = this.spawns.filter(spawn => spawn.spawning == null)
     const spawn = idleSpawns[0]
     if (spawn == null) {
       return
