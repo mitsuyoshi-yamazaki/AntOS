19557918c3f4eaa702c44eccf9384925e2cbf111
Yamazaki Mitsuyoshi <mitsuyoshi.tsukuba@gmail.com>
2021-08-06 17:29:42 +0900

    Invaderが死ぬ瞬間にhitsがmaxになる問題の対応

--- src/application/task/defence/safe_mode_manager_task.ts
+++ src/application/task/defence/safe_mode_manager_task.ts
@@ -6,11 +6,12 @@ import type { RoomName } from "utility/room_name"
 import { GameConstants } from "utility/constants"
 import { UnexpectedProblem } from "application/problem/unexpected/unexpected_problem"
 import { generateCodename } from "utility/unique_id"
-import { profileLink, roomLink } from "utility/log"
+import { profileLink, roomHistoryLink, roomLink } from "utility/log"
 import { OwnedRoomResource } from "room_resource/room_resource/owned_room_resource"
 import { DefenceTaskPerformance } from "application/task_profit/defence_task_performance"
 import { Invader } from "game/invader"
 import { PrimitiveLogger } from "os/infrastructure/primitive_logger"
+import { Timestamp } from "utility/timestamp"
 
 type SafeModeManagerTaskOutput = void
 type SafeModeManagerTaskProblemTypes = UnexpectedProblem
@@ -19,6 +20,8 @@ type SafeModeManagerTaskOutputs = TaskOutputs<SafeModeManagerTaskOutput, SafeMod
 export interface SafeModeManagerTaskState extends TaskState {
   /** task type identifier */
   readonly t: "SafeModeManagerTask"
+
+  hitsFullTimestamp: Timestamp | null
 }
 
 export class SafeModeManagerTask extends Task<SafeModeManagerTaskOutput, SafeModeManagerTaskProblemTypes, DefenceTaskPerformance> {
@@ -31,6 +34,7 @@ export class SafeModeManagerTask extends Task<SafeModeManagerTaskOutput, SafeMod
     startTime: number,
     sessionStartTime: number,
     roomName: RoomName,
+    private hitsFullTimestamp: Timestamp | null,
   ) {
     super(startTime, sessionStartTime, roomName)
 
@@ -44,15 +48,16 @@ export class SafeModeManagerTask extends Task<SafeModeManagerTaskOutput, SafeMod
       s: this.startTime,
       ss: this.sessionStartTime,
       r: this.roomName,
+      hitsFullTimestamp: this.hitsFullTimestamp,
     }
   }
 
   public static decode(state: SafeModeManagerTaskState): SafeModeManagerTask {
-    return new SafeModeManagerTask(state.s, state.ss, state.r)
+    return new SafeModeManagerTask(state.s, state.ss, state.r, state.hitsFullTimestamp)
   }
 
   public static create(roomName: RoomName): SafeModeManagerTask {
-    return new SafeModeManagerTask(Game.time, Game.time, roomName)
+    return new SafeModeManagerTask(Game.time, Game.time, roomName, null)
   }
 
   public run(roomResource: OwnedRoomResource): SafeModeManagerTaskOutputs {
@@ -62,11 +67,13 @@ export class SafeModeManagerTask extends Task<SafeModeManagerTaskOutput, SafeMod
     invaders.push(...roomResource.hostiles.creeps)
     invaders.push(...roomResource.hostiles.powerCreeps)
     if (invaders.length <= 0) {
+      this.hitsFullTimestamp = null
       return taskOutputs
     }
 
     if (roomResource.activeStructures.towers.length > 0) {
       if (roomResource.activeStructures.towers.some(tower => (tower.store.getUsedCapacity(RESOURCE_ENERGY) < (tower.store.getCapacity(RESOURCE_ENERGY) * 0.14)))) {
+        this.hitsFullTimestamp = null
         const result = roomResource.controller.activateSafeMode()
         switch (result) {
         case OK: {
@@ -97,6 +104,7 @@ export class SafeModeManagerTask extends Task<SafeModeManagerTaskOutput, SafeMod
       }
     }
 
+    let foundFullHpInvader = false as boolean
     const events = roomResource.room.getEventLog()
     for (const event of events) {
       if (event.event !== EVENT_ATTACK) {
@@ -109,6 +117,7 @@ export class SafeModeManagerTask extends Task<SafeModeManagerTaskOutput, SafeMod
         if (invader.hits < invader.hitsMax) {
           continue
         }
+        foundFullHpInvader = true
         if (invader.owner.username === Invader.username) {
           const closestTower = invader.pos.findClosestByRange(roomResource.activeStructures.towers)
           if (closestTower != null && invader.pos.getRangeTo(closestTower.pos) > 15) {
@@ -116,10 +125,18 @@ export class SafeModeManagerTask extends Task<SafeModeManagerTaskOutput, SafeMod
           }
         }
 
+        if (this.hitsFullTimestamp == null) {
+          continue
+        }
+        if ((Game.time - this.hitsFullTimestamp) <= 0) {
+          continue
+        }
+        this.hitsFullTimestamp = null
+
         const result = roomResource.controller.activateSafeMode()
         switch (result) {
         case OK: {
-          const message = `${roomLink(this.roomName)} activate safe mode (${invader}), attacker: ${profileLink(invader.owner.username)}, hits: ${invader.hits}, hitsMax: ${invader.hitsMax}`
+          const message = `${roomHistoryLink(this.roomName)} activate safe mode at ${Game.time} (${invader}), attacker: ${profileLink(invader.owner.username)}, hits: ${invader.hits}, hitsMax: ${invader.hitsMax}`
           PrimitiveLogger.fatal(message)
           taskOutputs.logs.push({
             taskIdentifier: this.identifier,
@@ -144,6 +161,14 @@ export class SafeModeManagerTask extends Task<SafeModeManagerTaskOutput, SafeMod
           return taskOutputs
         }
       }
+    }
+
+    if (foundFullHpInvader === true) {
+      if (this.hitsFullTimestamp == null) {
+        this.hitsFullTimestamp = Game.time
+      }
+    } else {
+      this.hitsFullTimestamp = null
     }
 
     return taskOutputs
