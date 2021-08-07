3056339dd35272099d76bb8eca62adc9d36a79cb
Yamazaki Mitsuyoshi <mitsuyoshi.tsukuba@gmail.com>
2021-08-08 01:00:14 +0900

    Remote Roomを追加

--- src/process/onetime/season_487837_attack_invader_core_room_names.ts
+++ src/process/onetime/season_487837_attack_invader_core_room_names.ts
@@ -39,7 +39,7 @@ export const remoteRoomNamesToDefend = ((): Map<RoomName, RoomName[]> => {
       ["W3S24", ["W3S23", "W3S25"]],
       ["W21S23", ["W21S22", "W22S23", "W21S24"]],
       ["W6S29", ["W5S29", "W7S29"]],
-      ["W6S27", ["W5S27"]],
+      ["W6S27", ["W5S27", "W7S27"]],
     ])
   }
 })()
--- src/utility/system_info.ts
+++ src/utility/system_info.ts
@@ -19,7 +19,7 @@ export const SystemInfo = {
     name: "AntOS",
   },
   application: {
-    version: "6.5.53",
+    version: "6.5.54",
     shortVersionString: ShortVersion.v6,
     name: "DecisionMaker",
   },
