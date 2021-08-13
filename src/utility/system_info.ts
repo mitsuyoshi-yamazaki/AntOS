8cfa41326192da422cf413f69fed09d0be4dfc10
Yamazaki Mitsuyoshi <mitsuyoshi.tsukuba@gmail.com>
2021-08-14 01:42:47 +0900

    UpgraderのLink利用を実装

--- src/utility/system_info.ts
+++ src/utility/system_info.ts
@@ -19,7 +19,7 @@ export const SystemInfo = {
     name: "AntOS",
   },
   application: {
-    version: "6.5.121",
+    version: "6.5.122",
     shortVersionString: ShortVersion.v6,
     name: "DecisionMaker",
   },
--- src/v5_object_task/creep_task/api_wrapper/get_energy_api_wrapper.ts
+++ src/v5_object_task/creep_task/api_wrapper/get_energy_api_wrapper.ts
@@ -7,7 +7,7 @@ import { CreepApiWrapperState } from "../creep_api_wrapper"
 import { EnergyStore } from "prototype/room_object"
 
 type GetEnergyApiWrapperResult = FINISHED | FINISHED_AND_RAN | ERR_NOT_IN_RANGE | ERR_BUSY | ERR_PROGRAMMING_ERROR
-type GetEnergyApiWrapperTargetType = EnergyStore
+type GetEnergyApiWrapperTargetType = EnergyStore | StructureLink
 
 export interface GetEnergyApiWrapperState extends CreepApiWrapperState {
   /** target id */
