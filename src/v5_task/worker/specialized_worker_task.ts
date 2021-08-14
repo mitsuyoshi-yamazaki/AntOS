d32d1496f16e2603811935e1f146ac6eddac21d6
Yamazaki Mitsuyoshi <mitsuyoshi.tsukuba@gmail.com>
2021-08-15 02:05:31 +0900

    Worker数をリセット

--- src/v5_task/worker/specialized_worker_task.ts
+++ src/v5_task/worker/specialized_worker_task.ts
@@ -44,7 +44,6 @@ export class SpecializedWorkerTask extends GeneralCreepWorkerTask {
 
     const numberOfCreeps: { [roomName: string]: number } = {
       "W6S29": 6,
-      "W21S23": 5,
 
       // shard3
       "W51S29": 4,
