f43cc463a12a26b891ccf0ca92e985648d518d0a
Yamazaki Mitsuyoshi <mitsuyoshi.tsukuba@gmail.com>
2021-08-18 14:32:00 +0900

    ターゲット選定を実装

--- src/utility/system_info.ts
+++ src/utility/system_info.ts
@@ -19,7 +19,7 @@ export const SystemInfo = {
     name: "AntOS",
   },
   application: {
-    version: "6.5.183",
+    version: "6.5.184",
     shortVersionString: ShortVersion.v6,
     name: "DecisionMaker",
   },
