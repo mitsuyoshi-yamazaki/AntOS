5597a27c4e9e5ece3194c4b1ee9bb151fed36ed8
Yamazaki Mitsuyoshi <mitsuyoshi.tsukuba@gmail.com>
2021-08-19 13:58:43 +0900

    ScoutがSafemodeのトリガーにならないよう修正

--- src/utility/system_info.ts
+++ src/utility/system_info.ts
@@ -19,7 +19,7 @@ export const SystemInfo = {
     name: "AntOS",
   },
   application: {
-    version: "6.5.186",
+    version: "6.5.190",
     shortVersionString: ShortVersion.v6,
     name: "DecisionMaker",
   },
