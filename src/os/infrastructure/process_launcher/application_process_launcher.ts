670e2bfe5cbe362f18b47591bd2c80e7d1630da2
Yamazaki Mitsuyoshi <mitsuyoshi.tsukuba@gmail.com>
2021-08-19 23:14:56 +0900

    V6RoomKeeperを常時起動するよう変更

--- src/os/infrastructure/process_launcher/application_process_launcher.ts
+++ src/os/infrastructure/process_launcher/application_process_launcher.ts
@@ -7,8 +7,10 @@ import { World } from "world_info/world_info"
 import { BootstrapRoomManagerProcess } from "process/bootstrap_room_manager_process"
 import type { Process } from "process/process"
 import type { ProcessLauncher } from "os/os_process_launcher"
-// import { V6RoomKeeperProcess } from "process/v6_room_keeper_process"
-// import { RoomKeeperTask } from "application/task/room_keeper/room_keeper_task"
+import { V6RoomKeeperProcess } from "process/v6_room_keeper_process"
+import { RoomKeeperTask } from "application/task/room_keeper/room_keeper_task"
+import { PrimitiveLogger } from "../primitive_logger"
+import { coloredText, roomLink } from "utility/log"
 
 export class ApplicationProcessLauncher {
   public launchProcess(processList: Process[], processLauncher: ProcessLauncher): void {
@@ -23,28 +25,30 @@ export class ApplicationProcessLauncher {
       }
       return null
     })
-    // const roomsWithV6KeeperProcess = processList.map(process => {
-    //   if (process instanceof V6RoomKeeperProcess) {
-    //     return process.roomName
-    //   }
-    //   return null
-    // })
+    const roomsWithV6KeeperProcess = processList.map(process => {
+      if (process instanceof V6RoomKeeperProcess) {
+        return process.roomName
+      }
+      return null
+    })
 
     World.rooms.getAllOwnedRooms().forEach(room => {
       switch (Migration.roomVersion(room.name)) {
       case ShortVersion.v3:
         return
       case ShortVersion.v5:
-        if (roomsWithV5KeeperProcess.includes(room.name) === true) {
-          return
+        if (roomsWithV5KeeperProcess.includes(room.name) !== true) {
+          this.launchV5RoomKeeperProcess(room.name, processLauncher)
         }
-        this.launchV5RoomKeeperProcess(room.name, processLauncher)
+        if (roomsWithV6KeeperProcess.includes(room.name) !== true) {
+          this.launchV6RoomKeeperProcess(room.name, processLauncher)  // v6に移行途中
+        }
         return
       case ShortVersion.v6:
-        // if (roomsWithV6KeeperProcess.includes(room.name) === true) {
-        //   return
-        // }
-        // this.launchV6RoomKeeperProcess(room.name, processLauncher)
+        if (roomsWithV6KeeperProcess.includes(room.name) === true) {
+          return
+        }
+        this.launchV6RoomKeeperProcess(room.name, processLauncher)
         return
       }
     })
@@ -55,10 +59,11 @@ export class ApplicationProcessLauncher {
     processLauncher(processId => RoomKeeperProcess.create(processId, roomKeeperTask))
   }
 
-  // private launchV6RoomKeeperProcess(roomName: RoomName, processLauncher: ProcessLauncher): void {
-  //   const roomKeeperTask = RoomKeeperTask.create(roomName)
-  //   processLauncher(processId => V6RoomKeeperProcess.create(processId, roomKeeperTask))
-  // }
+  private launchV6RoomKeeperProcess(roomName: RoomName, processLauncher: ProcessLauncher): void {
+    PrimitiveLogger.log(`${coloredText("[Launched]", "info")} V6RoomKeeperProcess ${roomLink(roomName)}`)
+    const roomKeeperTask = RoomKeeperTask.create(roomName)
+    processLauncher(processId => V6RoomKeeperProcess.create(processId, roomKeeperTask))
+  }
 
   private checkBoostrapRoomManagerProcess(processList: Process[], processLauncher: ProcessLauncher): void {
     if (processList.some(process => process instanceof BootstrapRoomManagerProcess) === true) {
