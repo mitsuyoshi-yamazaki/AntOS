b8a3020dce85b46f9cbf816e4e0cb6ef6a7cd079
Yamazaki Mitsuyoshi <mitsuyoshi.tsukuba@gmail.com>
2021-08-20 23:42:58 +0900

    UpgraderがContainerの届かない場所に配置される問題を修正

--- src/utility/system_info.ts
+++ src/utility/system_info.ts
@@ -19,7 +19,7 @@ export const SystemInfo = {
     name: "AntOS",
   },
   application: {
-    version: "6.5.195",
+    version: "6.5.197",
     shortVersionString: ShortVersion.v6,
     name: "DecisionMaker",
   },
