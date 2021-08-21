db0037f76213d3fbdd4b90ad69b11821d7b91f3a
Yamazaki Mitsuyoshi <mitsuyoshi.tsukuba@gmail.com>
2021-08-21 21:07:10 +0900

    バージョンを更新

--- src/utility/system_info.ts
+++ src/utility/system_info.ts
@@ -19,7 +19,7 @@ export const SystemInfo = {
     name: "AntOS",
   },
   application: {
-    version: "6.5.198",
+    version: "6.6.0",
     shortVersionString: ShortVersion.v6,
     name: "DecisionMaker",
   },
