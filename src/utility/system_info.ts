eb73f6705c7c54cbc79345e34ecbfe7b7e814172
Yamazaki Mitsuyoshi <mitsuyoshi.tsukuba@gmail.com>
2021-08-30 03:01:58 +0900

    Resource分配を実装

--- src/utility/system_info.ts
+++ src/utility/system_info.ts
@@ -19,7 +19,7 @@ export const SystemInfo = {
     name: "AntOS",
   },
   application: {
-    version: "6.7.7",
+    version: "6.7.8",
     shortVersionString: ShortVersion.v6,
     name: "DecisionMaker",
   },
