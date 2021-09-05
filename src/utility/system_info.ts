c31faad4fe487255035714d0b657201fef780d07
Yamazaki Mitsuyoshi <mitsuyoshi.tsukuba@gmail.com>
2021-09-06 00:49:58 +0900

    部屋の境界にRoadを設置する挙動を削除

--- src/utility/system_info.ts
+++ src/utility/system_info.ts
@@ -16,7 +16,7 @@ export const SystemInfo = {
     name: "AntOS",
   },
   application: {
-    version: "7.0.14",
+    version: "7.0.15",
     shortVersionString: ShortVersion.v6,
     name: "Declarative AI",
   },
