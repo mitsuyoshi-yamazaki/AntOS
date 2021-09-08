0653e2e4f5f4633dab2c1b2668722451d042813f
Yamazaki Mitsuyoshi <mitsuyoshi.tsukuba@gmail.com>
2021-09-09 00:13:16 +0900

    v7バージョンを追加

--- src/utility/system_info.ts
+++ src/utility/system_info.ts
@@ -1,13 +1,16 @@
 export type ShortVersionV5 = "v5"
 export type ShortVersionV6 = "v6"
+export type ShortVersionV7 = "v7"
 
 const shortVersionV5: ShortVersionV5 = "v5"
 const shortVersionV6: ShortVersionV6 = "v6"
+const shortVersionV7: ShortVersionV7 = "v7"
 
-export type ShortVersion = ShortVersionV5 | ShortVersionV6
+export type ShortVersion = ShortVersionV5 | ShortVersionV6 | ShortVersionV7
 export const ShortVersion = {
   v5: shortVersionV5,
   v6: shortVersionV6,
+  v7: shortVersionV7,
 }
 
 export const SystemInfo = {
@@ -16,8 +19,8 @@ export const SystemInfo = {
     name: "AntOS",
   },
   application: {
-    version: "7.1.0",
-    shortVersionString: ShortVersion.v6,
+    version: "7.1.1",
+    shortVersionString: ShortVersion.v7,
     name: "Declarative AI",
   },
 }
