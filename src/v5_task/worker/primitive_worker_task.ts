2caa215b2914371ba82c8cb3fec70be275fcbbb7
Yamazaki Mitsuyoshi <mitsuyoshi.tsukuba@gmail.com>
2022-01-16 05:21:29 +0900

    RCL1のcreep数上限を変更

--- src/v5_task/worker/primitive_worker_task.ts
+++ src/v5_task/worker/primitive_worker_task.ts
@@ -191,8 +191,13 @@ export class PrimitiveWorkerTask extends Task {
     }
 
     const requiredCreepCount = getRequiredCreepCount(ownedRoomSourceEnergyCapacity, 3) + getRequiredCreepCount(estimatedNeighbourSourceEnergyCapacity, 4)
-    const maxCreepCount = objects.sources.length * 8 + neighbourRoomSourceCount * 10
-    const creepCount = Math.min(requiredCreepCount, maxCreepCount, 60)
+    const maxCreepCount = ((): number => {
+      if (objects.controller.level <= 1) {
+        return 12
+      }
+      return objects.sources.length * 8 + neighbourRoomSourceCount * 10
+    })()
+    const creepCount = Math.min(requiredCreepCount, maxCreepCount, 30)
 
     return {
       creepCount,
