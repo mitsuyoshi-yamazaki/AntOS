1b7d4a3f0bcd9d488464dd6e17c50af189aa4f84
Yamazaki Mitsuyoshi <mitsuyoshi.tsukuba@gmail.com>
2022-01-22 21:06:06 +0900

    CommodityはTerminalに保管

--- src/utility/system_info.ts
+++ src/utility/system_info.ts
@@ -19,7 +19,7 @@ export const SystemInfo = {
     name: "AntOS",
   },
   application: {
-    version: "7.2.71",
+    version: "7.2.74",
     shortVersionString: ShortVersion.v7,
     name: "Declarative AI",
   },
