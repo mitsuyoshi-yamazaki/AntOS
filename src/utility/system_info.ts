c29dced42a7971e2d8f2766e82baf1542ed9ee65
Yamazaki Mitsuyoshi <mitsuyoshi.tsukuba@gmail.com>
2022-01-28 00:06:20 +0900

    v8.0.0

--- src/utility/system_info.ts
+++ src/utility/system_info.ts
@@ -19,7 +19,7 @@ export const SystemInfo = {
     name: "AntOS",
   },
   application: {
-    version: "7.2.132",
+    version: "8.0.0",
     shortVersionString: ShortVersion.v7,
     name: "Declarative AI",
   },
