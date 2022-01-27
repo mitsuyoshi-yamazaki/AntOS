e6c7fffa82ed51d06a890244aff90d4bd887e5e1
Yamazaki Mitsuyoshi <mitsuyoshi.tsukuba@gmail.com>
2022-01-28 02:47:20 +0900

    submoduleを更新

--- src/utility/system_info.ts
+++ src/utility/system_info.ts
@@ -19,7 +19,7 @@ export const SystemInfo = {
     name: "AntOS",
   },
   application: {
-    version: "8.0.3",
+    version: "8.0.4",
     shortVersionString: ShortVersion.v7,
     name: "Declarative AI",
   },
