ee7b9b11ac5a447945b408add7785b7fac3d1042
Yamazaki Mitsuyoshi <mitsuyoshi.tsukuba@gmail.com>
2022-01-29 21:52:49 +0900

    Creepを生成できない問題を修正

--- src/process/temporary/season4_275982_harvest_commodity_process.ts
+++ src/process/temporary/season4_275982_harvest_commodity_process.ts
@@ -240,7 +240,7 @@ export class Season4275982HarvestCommodityProcess implements Process, Procedural
       MOVE, MOVE, MOVE, MOVE, MOVE,
       WORK, WORK, WORK, WORK, WORK,
     ]
-    const body = CreepBody.create(baseBody, bodyUnit, roomResources.room.energyCapacityAvailable, 5)
+    const body = CreepBody.create(baseBody, bodyUnit, roomResources.room.energyCapacityAvailable, 4)
     World.resourcePools.addSpawnCreepRequest(this.parentRoomName, {
       priority: CreepSpawnRequestPriority.Low,
       numberOfCreeps: 1,
