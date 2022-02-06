fc7ca4d918f65361ef3779cfab11f129a2b3b89a
Yamazaki Mitsuyoshi <mitsuyoshi.tsukuba@gmail.com>
2022-02-06 23:25:31 +0900

    Remote Room Infoマイグレーション処理を追加

--- src/utility/system_info.ts
+++ src/utility/system_info.ts
@@ -19,7 +19,7 @@ export const SystemInfo = {
     name: "AntOS",
   },
   application: {
-    version: "8.2.0",
+    version: "8.2.1",
     shortVersionString: ShortVersion.v7,
     name: "Declarative AI",
   },
