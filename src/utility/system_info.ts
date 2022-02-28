24e225454cc9ff99c1edb1eeafef3a3c8bf2d584
Yamazaki Mitsuyoshi <mitsuyoshi.tsukuba@gmail.com>
2022-02-28 12:39:35 +0900

    Wall破壊コマンドを追加

--- src/utility/system_info.ts
+++ src/utility/system_info.ts
@@ -19,7 +19,7 @@ export const SystemInfo = {
     name: "AntOS",
   },
   application: {
-    version: "8.3.71",
+    version: "8.3.72",
     shortVersionString: ShortVersion.v7,
     name: "Declarative AI",
   },
