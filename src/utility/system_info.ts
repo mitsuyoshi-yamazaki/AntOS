f6eb0fd587f35df670ce5353d7ea8e831445cea5
Yamazaki Mitsuyoshi <mitsuyoshi.tsukuba@gmail.com>
2022-02-28 12:13:49 +0900

    Power収集スクリプトを実装

--- src/utility/system_info.ts
+++ src/utility/system_info.ts
@@ -19,7 +19,7 @@ export const SystemInfo = {
     name: "AntOS",
   },
   application: {
-    version: "8.3.70",
+    version: "8.3.71",
     shortVersionString: ShortVersion.v7,
     name: "Declarative AI",
   },
