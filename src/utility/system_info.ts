a928d1431dbc4fcaa55efe8adddbc4b61c3e285d
Yamazaki Mitsuyoshi <mitsuyoshi.tsukuba@gmail.com>
2022-03-19 01:00:39 +0900

    全体メモリ使用量確認コマンドを追加

--- src/utility/system_info.ts
+++ src/utility/system_info.ts
@@ -19,7 +19,7 @@ export const SystemInfo = {
     name: "AntOS",
   },
   application: {
-    version: "8.4.38",
+    version: "8.4.39",
     shortVersionString: ShortVersion.v7,
     name: "Declarative AI",
   },
