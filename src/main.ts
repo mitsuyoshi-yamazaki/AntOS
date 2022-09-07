e523ac166970f208f174c45faf9a4187ec22fa48
Yamazaki Mitsuyoshi <mitsuyoshi.tsukuba@gmail.com>
2022-09-08 00:19:38 +0900

    サーバーリセット時のログを変更

--- src/main.ts
+++ src/main.ts
@@ -4,18 +4,15 @@ import { ErrorMapper } from "error_mapper/ErrorMapper"
 import { memhack } from "./memory_hack"
 // import * as ScreepsProfiler from "screeps-profiler" // Game.profiler.profile(ticks)
 
-import { leveled_colored_text } from "./utility"
-import { SystemInfo } from "shared/utility/system_info"
 import { BootLoader } from "./boot_loader"
+import { ConsoleUtility } from "shared/utility/console_utility/console_utility"
 
+console.log(ConsoleUtility.colored(`Rebooted at ${Game.time}`, "warn"))
 memhack.load()
 
 const rootFunctions = BootLoader.load()
 rootFunctions.load()
 const mainLoop = rootFunctions.loop
-
-const initializing_message = `${SystemInfo.os.name} v${SystemInfo.os.version} - ${SystemInfo.application.name} v${SystemInfo.application.version} reboot in ${Game.shard.name} at ${Game.time}`
-console.log(leveled_colored_text(initializing_message, "warn"))
 
 // ScreepsProfiler.enable()  // TODO: 普段はオフに
 
