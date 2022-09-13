98c64c62cbbcaf3dc0b48fc1e0f66d7018644ed4
Yamazaki Mitsuyoshi <mitsuyoshi.tsukuba@gmail.com>
2022-09-13 17:58:26 +0900

    Terminalが満杯の時に停止する問題を修正

--- src/shared/utility/system_info.ts
+++ src/shared/utility/system_info.ts
@@ -22,7 +22,7 @@ export const SystemInfo = {
     name: "AntOS",
   },
   application: {
-    version: "9.1.6",
+    version: "9.1.19",
     name: "Declarative AI",
   },
 }
--- src/v5_task/worker/specialized_worker_task.ts
+++ src/v5_task/worker/specialized_worker_task.ts
@@ -230,7 +230,12 @@ export class SpecializedWorkerTask extends GeneralCreepWorkerTask {
 
       const storedResourceType = (Array.from(Object.keys(creep.store)) as ResourceConstant[])[0]
       if (storedResourceType != null) {
-        const transferTarget = objects.activeStructures.terminal ?? objects.activeStructures.storage
+        const transferTarget = ((): StructureTerminal | StructureStorage | null => {
+          if (objects.activeStructures.terminal != null && objects.activeStructures.terminal.store.getFreeCapacity() > 0) {
+            return objects.activeStructures.terminal
+          }
+          return objects.activeStructures.storage
+        })()
         if (transferTarget != null) {
           return MoveToTargetTask.create(TransferResourceApiWrapper.create(transferTarget, storedResourceType))
         }
