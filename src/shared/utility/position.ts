6da75868008d0a25ae301839dd36730a7ffbeec2
Yamazaki Mitsuyoshi <mitsuyoshi.tsukuba@gmail.com>
2022-10-02 16:07:26 +0900

    isEqualLocalPositionを追加

--- src/shared/utility/position.ts
+++ src/shared/utility/position.ts
@@ -2,3 +2,7 @@ export type Position = {
   readonly x: number
   readonly y: number
 }
+
+export const isEqualLocalPosition = (position1: Position, position2: Position): boolean => {
+  return position1.x === position2.x && position1.y === position2.y
+}
