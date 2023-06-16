5529a61bbfa4fb5674bf77651f425b86ef6b59d6
Yamazaki Mitsuyoshi <mitsuyoshi.tsukuba@gmail.com>
2023-06-17 00:25:46 +0900

    次期OSのでも実装を追加

---
+++ src/script/demo/demo_os/os.ts
@@ -0,0 +1,104 @@
+/**
+ # 構造（要件）
+ - プラットフォーム操作APIとしてSystemCall, Driverが用意される
+   - SystemCallは相互に依存しない（のが望ましい。可能か？）
+ - SystemCallを利用してOSが実装される
+ - SystemCallとDriverを利用してProcessが実装される
+   - →ProcessはSystemCallと依存するDriverへのアクセスが与えられている
+
+ # 構造（仕様）
+ - DriverはOSに渡す段階で固定して、ProcessはOSを経由してそのDriverへ依存するようにしたらどうか
+   - → DIコンテナの使い所では
+ */
+
+type SystemCall = {
+  load(): void
+  startOfTick(): void
+  endOfTick(): void
+}
+
+type SystemCalls = {}
+
+type Driver = {
+  load(): void
+  startOfTick(): void
+  endOfTick(): void
+}
+
+declare namespace Tag {
+  const OpaqueTagSymbol: unique symbol;
+
+  class OpaqueTag<T> {
+    private [OpaqueTagSymbol]: T;
+  }
+}
+type ProcessId<T extends Process> = string & Tag.OpaqueTag<T>;
+
+// ProcessのdependencyはDriverだけではなく親Processが渡す引数が入る可能性がある
+// それをProcessManagerはどう平準化するか？
+interface Process {
+  readonly processId: ProcessId<this>
+
+
+}
+
+// ProcessからProcessManagerを呼び出す経路が循環しないようにする
+class ProcessManager implements SystemCall {  // TODO: プラットフォームの機能ではないので、OSのサブシステム扱いにする
+  private processes: Process[] = []
+
+  public load(): void {
+    this.restoreProcesses()
+  }
+
+  public startOfTick(): void {
+  }
+
+  public endOfTick(): void {
+    this.storeProcesses()
+  }
+
+  public addProcess(process: Process): void {
+  }
+
+  public getProcess<P extends Process>(processId: ProcessId<P>): P | null {
+    return null // TODO:
+  }
+
+  private restoreProcesses(): void {
+  }
+
+  private storeProcesses(): void {
+  }
+}
+
+class OperatingSystem<S extends SystemCalls> {
+  public constructor(
+    public readonly systemCall: S,
+    public readonly drivers: Driver[],
+  ) {
+  }
+
+  public load(): void {
+  }
+
+  public startOfTick(): void {
+  }
+
+  public endOfTick(): void {
+  }
+}
+
+type ScreepsSystemCall = SystemCalls
+const screepsSystemCall: ScreepsSystemCall = {
+  load(): void {
+  },
+
+  startOfTick(): void {
+  },
+
+  endOfTick(): void {
+  },
+}
+
+class ScreepsOS extends OperatingSystem<ScreepsSystemCall> {
+}
