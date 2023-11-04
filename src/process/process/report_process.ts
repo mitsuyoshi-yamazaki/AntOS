d2ed9b05b7e7247ff68660c013b1054223d9e7d4
Yamazaki Mitsuyoshi <mitsuyoshi.tsukuba@gmail.com>
2023-11-04 19:47:54 +0900

    中身を実装

--- src/process/process/report_process.ts
+++ src/process/process/report_process.ts
@@ -2,12 +2,15 @@ import { Procedural } from "process/procedural"
 import { Process, ProcessId } from "process/process"
 import { ProcessState } from "../process_state"
 import { ProcessDecoder } from "../process_decoder"
+import { ConsoleUtility } from "shared/utility/console_utility/console_utility"
 
 ProcessDecoder.register("ReportProcess", state => {
   return ReportProcess.decode(state as ReportProcessState)
 })
 
 interface ReportProcessState extends ProcessState {
+  readonly reportTimeHour: number
+  readonly reportedDay: number
 }
 
 export class ReportProcess implements Process, Procedural {
@@ -19,6 +22,8 @@ export class ReportProcess implements Process, Procedural {
   private constructor(
     public readonly launchTime: number,
     public readonly processId: ProcessId,
+    public readonly reportTimeHour: number,
+    public readonly reportedDay: number,
   ) {
     this.identifier = `ReportProcess${launchTime}`
   }
@@ -28,22 +33,59 @@ export class ReportProcess implements Process, Procedural {
       t: "ReportProcess",
       l: this.launchTime,
       i: this.processId,
+      reportTimeHour: this.reportTimeHour,
+      reportedDay: this.reportedDay,
     }
   }
 
   public static decode(state: ReportProcessState): ReportProcess {
-    return new ReportProcess(state.l, state.i)
+    return new ReportProcess(
+      state.l,
+      state.i,
+      state.reportTimeHour ?? 0,
+      state.reportedDay ?? 0
+    )
   }
 
   public static create(processId: ProcessId): ReportProcess {
-    return new ReportProcess(Game.time, processId)
+    return new ReportProcess(Game.time, processId, 0, 0)
   }
 
   public processShortDescription(): string {
-    return "" // TODO:
+    const descriptions: string[] = [
+      `report hour: ${this.reportTimeHour}`,
+      `time to next report: ${(new Date()).getHours() + 24 - this.reportTimeHour}`,
+    ]
+
+    return descriptions.join(", ")
+  }
+
+  public didReceiveMessage(message: string): string {
+    const commandList = ["help", "test"]
+    const components = message.split(" ")
+    const command = components.shift()
+
+    try {
+      switch (command) {
+      case "help":
+        return `Commands: ${commandList}`
+
+      case "test":
+        return this.processShortDescription()
+
+      default:
+        throw `Invalid command ${commandList}. see "help"`
+      }
+    } catch (error) {
+      return `${ConsoleUtility.colored("[ERROR]", "error")} ${error}`
+    }
   }
 
   public runOnTick(): void {
+
+  }
+
+  private report(): void {
 
   }
 }
