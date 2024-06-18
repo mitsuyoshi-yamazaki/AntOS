b8380ab0f7826136b225b40ccddd770b1f8bf4bb
Yamazaki Mitsuyoshi <mitsuyoshi.tsukuba@gmail.com>
2024-06-18 22:01:38 +0900

    UnionTypeの制約を追加

--- src/shared/utility/types.ts
+++ src/shared/utility/types.ts
@@ -3,3 +3,11 @@ export type Mutable<T> = {
 }
 
 export type ElementType<T> = T extends (infer U)[] ? U : never;
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export type IsUnion<T, U = T> = T extends any
+  ? [U] extends [T]
+  ? false : true
+  : never
+
+export type NotUnion<T> = IsUnion<T> extends true ? never : T
