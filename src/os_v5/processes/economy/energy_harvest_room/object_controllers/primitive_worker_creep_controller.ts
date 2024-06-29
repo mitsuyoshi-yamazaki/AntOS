b571ac84ddcc78fa2c82db75e6aaca49d8ffb51a
Yamazaki Mitsuyoshi <mitsuyoshi.tsukuba@gmail.com>
2024-06-30 08:29:45 +0900

    PrimitiveWorkerCreepControllerがTaskを使うように変更

--- src/os_v5/processes/economy/energy_harvest_room/object_controllers/primitive_worker_creep_controller.ts
+++ src/os_v5/processes/economy/energy_harvest_room/object_controllers/primitive_worker_creep_controller.ts
@@ -1,5 +1,6 @@
 import { CreepTask } from "os_v5/processes/game_object_management/creep/creep_task/creep_task"
 import { RoomName } from "shared/utility/room_name_types"
+import { GameConstants } from "utility/constants"
 import { EnergyHarvestRoomProcessCreep } from "../types"
 
 export class PrimitiveWorkerCreepController {
@@ -9,8 +10,31 @@ export class PrimitiveWorkerCreepController {
     if (creep.task != null) {
       return
     }
-    // creep.task = this.workerTaskFor(creep)
-    this.runDirectly(creep, roomName)
+    creep.task = this.workerTaskFor(creep, roomName)
+    // this.runDirectly(creep, roomName)
+  }
+
+  private workerTaskFor(creep: EnergyHarvestRoomProcessCreep, roomName: RoomName): CreepTask.AnyTask | null {
+    if (creep.room.name !== roomName) {
+      return CreepTask.Tasks.MoveToRoom.create(roomName, [])
+    }
+
+    const source = creep.pos.findClosestByRange(FIND_SOURCES_ACTIVE)
+    if (source == null) {
+      return null // FixMe: 一度に全ての状態を設定するので、タスク状態とCreepMemory両方で無駄が発生している
+    }
+    const controller = creep.room.controller
+    if (controller == null) {
+      return null
+    }
+
+    const tasks: CreepTask.AnyTask[] = [
+      CreepTask.Tasks.MoveTo.create(source.pos, GameConstants.creep.actionRange.harvest),
+      CreepTask.Tasks.HarvestEnergy.create(source.id),
+      CreepTask.Tasks.MoveTo.create(controller.pos, GameConstants.creep.actionRange.upgradeController),
+      CreepTask.Tasks.UpgradeController.create(controller.id),
+    ]
+    return CreepTask.Tasks.Sequential.create(tasks)
   }
 
   private runDirectly(creep: EnergyHarvestRoomProcessCreep, roomName: RoomName): void {
