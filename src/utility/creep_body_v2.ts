e725b2427a12d80231c614a398eb35ad43865789
Yamazaki Mitsuyoshi <mitsuyoshi.tsukuba@gmail.com>
2024-06-29 19:12:19 +0900

    文字列表現からの復元処理を追加

--- src/utility/creep_body_v2.ts
+++ src/utility/creep_body_v2.ts
@@ -1,9 +1,57 @@
-export class CreepBody {
-  public readonly bodyParts: BodyPartConstant[]
+import { reverseConstMapping } from "shared/utility/strict_entries"
 
-  // public constructor(bodyDefinition: string)
-  public constructor(bodyParts: BodyPartConstant[]) {
-  // public constructor(args: string | BodyPartConstant[]) { // TODO:
-    this.bodyParts = bodyParts
+const bodyPartEncodingMap = {
+  w: WORK,
+  a: ATTACK,
+  ra: RANGED_ATTACK,
+  h: HEAL,
+  c: CARRY,
+  cl: CLAIM,
+  m: MOVE,
+  t: TOUGH,
+} as const
+const bodyPartDecodingMap = reverseConstMapping(bodyPartEncodingMap)
+
+export class CreepBody {
+  private constructor(
+    public readonly bodyParts: BodyPartConstant[]
+  ) {
   }
+
+  public static createWithBodyParts(bodyParts: BodyPartConstant[]): CreepBody {
+    return new CreepBody(bodyParts)
+  }
+
+  /**
+   * @throws
+   * @param body {parts count}{parts type}... ex: 3w3c6m
+   */
+  public static createFromTextRepresentation(input: string): CreepBody {
+    return new CreepBody(parseEncodedCreepBody(input))
+  }
+}
+
+
+/** @throws */
+const parseEncodedCreepBody = (input: string): BodyPartConstant[] => {
+  const result: [number, string][] = []
+  const regex = /(\d+)([a-zA-Z]+)/g
+  let match: RegExpExecArray | null = null
+
+  while ((match = regex.exec(input)) !== null) {
+    if (match[1] == null || match[2] == null) {
+      throw `Input string "${input}" is not number & string pair`
+    }
+    const numberPart = parseInt(match[1], 10)
+    const stringPart = match[2]
+    result.push([numberPart, stringPart])
+  }
+
+  return result.flatMap(([count, encodedBodyPart], index): BodyPartConstant[] => {
+    const bodyPart = (bodyPartEncodingMap as Record<string, BodyPartConstant>)[encodedBodyPart.toLowerCase()]
+    if (bodyPart == null) {
+      throw `Invalid body part specifier ${encodedBodyPart} (in ${input} at ${index})`
+    }
+    return new Array(count).map(_ => bodyPart)
+  })
 }
