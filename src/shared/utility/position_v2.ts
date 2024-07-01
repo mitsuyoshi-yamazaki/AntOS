27b3c2a79501052bb566160f0f1c7adb39ef7896
Yamazaki Mitsuyoshi <mitsuyoshi.tsukuba@gmail.com>
2024-07-02 00:58:36 +0900

    更新

--- src/shared/utility/position_v2.ts
+++ src/shared/utility/position_v2.ts
@@ -1,10 +1,17 @@
 import { AvailableRoomPositions } from "./room_position"
+import { Position as AnyPosition } from "./position"
 
 export type Position = {
   readonly x: AvailableRoomPositions
   readonly y: AvailableRoomPositions
 }
 
-export const isEqualLocalPosition = (position1: Position, position2: Position): boolean => {
+export const isEqualLocalPosition = (position1: AnyPosition, position2: AnyPosition): boolean => {
   return position1.x === position2.x && position1.y === position2.y
 }
+
+export const positionDistance = (position1: AnyPosition, position2: AnyPosition): number => {
+  return Math.max(Math.abs(position1.x - position2.x), Math.abs(position1.y - position2.y))
+}
+
+export { AnyPosition }
