2be4b363e4cd77ad0202f55127949b4866eb3a08
Yamazaki Mitsuyoshi <mitsuyoshi.tsukuba@gmail.com>
2024-07-13 16:53:53 +0900

    定期処理実行処理を追加

--- src/os_v5/kernel.ts
+++ src/os_v5/kernel.ts
@@ -32,7 +32,7 @@ export const Kernel: KernelLifecycle<KernelMemory> & Kernel = {
   [Symbol.toStringTag]: "Kernel",
 
   name: "AntOS",
-  version: new SemanticVersion(5, 8, 1),
+  version: new SemanticVersion(5, 8, 2),
   launchedAt: {
     time: Game.time,
     datetime: new Date(),
--- src/os_v5/system_calls/depended_system_calls/scheduled_static_task_manager.ts
+++ src/os_v5/system_calls/depended_system_calls/scheduled_static_task_manager.ts
@@ -1,15 +1,29 @@
+import { ErrorMapper } from "error_mapper/ErrorMapper"
 import { AnyProcessId } from "os_v5/process/process"
 import { SystemCall } from "os_v5/system_call"
 import { EmptySerializable } from "os_v5/utility/types"
+import { ValuedArrayMap } from "shared/utility/valued_collection"
 
-type ScheduledStaticTaskInterval = "10" | "100" | "1000" | "10000" | "50000"
+// ScheduledStaticTaskManagerで管理することのオーバーヘッドがあるため、短期間の定期処理は行わない
+type ScheduledStaticTaskInterval = "100" | "1000" | "10000" | "50000"
 
 type ScheduledStaticTaskManager = {
   add(processId: AnyProcessId, interval: ScheduledStaticTaskInterval, task: () => void, options?: {canSkip?: 1|2|4}): void
 }
 
-// const taskQueue =
-// const tasks =
+type TaskId = number
+type Task = {
+  readonly taskId: TaskId
+  readonly processId: AnyProcessId
+  readonly interval: ScheduledStaticTaskInterval
+  readonly task: () => void
+  readonly canSkip: 0|1|2|4
+}
+
+let taskIdIndex = 0
+const taskQueue: Task[] = []
+const taskIdsInQueue = new Set<TaskId>()
+const allTasks = new ValuedArrayMap<ScheduledStaticTaskInterval, Task>()
 
 
 export const ScheduledStaticTaskManager: SystemCall<"ScheduledStaticTaskManager", EmptySerializable> & ScheduledStaticTaskManager = {
@@ -23,10 +37,64 @@ export const ScheduledStaticTaskManager: SystemCall<"ScheduledStaticTaskManager"
   },
 
   endOfTick(): EmptySerializable {
+    if (Game.time % 97 === 0) {
+      addTasksToQueue(allTasks.getValueFor("100"))
+    }
+    if (Game.time % 997 === 0) {
+      addTasksToQueue(allTasks.getValueFor("1000"))
+    }
+    if (Game.time % 9973 === 0) {
+      addTasksToQueue(allTasks.getValueFor("10000"))
+    }
+    if (Game.time % 49999 === 0) {
+      addTasksToQueue(allTasks.getValueFor("50000"))
+    }
+
+    runTasks()
+
     return {}
   },
 
   // ScheduledStaticTaskManager
   add(processId: AnyProcessId, interval: ScheduledStaticTaskInterval, task: () => void, options?: { canSkip?: 1 | 2 | 4 }): void {
+    allTasks.getValueFor(interval).push({
+      taskId: taskIdIndex,
+      processId,
+      interval,
+      task,
+      canSkip: options?.canSkip ?? 0,
+    })
+
+    taskIdIndex += 1
   },
+}
+
+const addTasksToQueue = (tasks: Task[]): void => {
+  tasks.forEach(task => {
+    if (taskIdsInQueue.has(task.taskId) === true) {
+      return
+    }
+    taskIdsInQueue.add(task.taskId)
+    taskQueue.push(task)
+  })
+}
+
+const runTasks = (): void => {
+  if (taskQueue.length <= 0) {
+    return
+  }
+  if (Game.cpu.bucket < 9500) {
+    return
+  }
+
+  const cpuLimit = Game.cpu.limit
+
+  for (const task of taskQueue) {
+    if (cpuLimit - Game.cpu.getUsed() < 20) {
+      return
+    }
+    ErrorMapper.wrapLoop((): void => {
+      task.task()
+    }, `ScheduledStaticTaskManager.runTasks(${task.processId}, interval: ${task.interval})`)()
+  }
 }
