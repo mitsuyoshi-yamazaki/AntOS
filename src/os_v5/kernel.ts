01c75b97642203cba2f292174ab9ecf2c7079021
Yamazaki Mitsuyoshi <mitsuyoshi.tsukuba@gmail.com>
2024-07-25 19:48:57 +0900

    バージョンを更新

--- src/os_v5/kernel.ts
+++ src/os_v5/kernel.ts
@@ -32,7 +32,7 @@ export const Kernel: KernelLifecycle<KernelMemory> & Kernel = {
   [Symbol.toStringTag]: "Kernel",
 
   name: "AntOS",
-  version: new SemanticVersion(5, 10, 9),
+  version: new SemanticVersion(5, 11, 0),
   launchedAt: {
     time: Game.time,
     datetime: new Date(),
