872e7abb051d5304ddded495ba917f6775858d69
Yamazaki Mitsuyoshi <mitsuyoshi.tsukuba@gmail.com>
2024-07-28 00:05:53 +0900

    Spawn予約機能を追加

--- src/os_v5/processes/v3_os_bridge/v3_bridge_spawn_request_process.ts
+++ src/os_v5/processes/v3_os_bridge/v3_bridge_spawn_request_process.ts
@@ -1,17 +1,18 @@
-import { Process, processDefaultIdentifier, ProcessDefaultIdentifier, ProcessDependencies, ProcessId, ReadonlySharedMemory } from "../../process/process"
+import { AnyProcessId, Process, processDefaultIdentifier, ProcessDefaultIdentifier, ProcessDependencies, ProcessId, ReadonlySharedMemory } from "../../process/process"
 import { ProcessDecoder } from "os_v5/system_calls/process_manager/process_decoder"
-import { EmptySerializable, SerializableObject } from "os_v5/utility/types"
-import { CreepBody } from "utility/creep_body_v2"
+import { SerializableObject } from "os_v5/utility/types"
+import { CreepBody, CreepBodyStringRepresentation } from "utility/creep_body_v2"
 import { RoomName } from "shared/utility/room_name_types"
 import { SystemCalls } from "os_v5/system_calls/interface"
 import { ConsoleUtility } from "shared/utility/console_utility/console_utility"
 import { CreepName } from "prototype/creep"
 import { V5CreepMemory } from "os_v5/utility/game_object/creep"
 import { V3BridgeDriverProcessApi } from "./v3_bridge_driver_process"
-import {} from "os_v5/utility/onheap_logger"
+import { Notification } from "os_v5/system_calls/depended_system_calls/notification_manager_types"
 
 // SpawnPoolのライフサイクル（v3 OSの実行）が終わった後に実行する必要がある
 
+// TODO: リクエストを補完して、Spawnしたら通知する
 
 type SpawnRequest = {
   readonly body: CreepBody
@@ -22,18 +23,54 @@ type SpawnRequest = {
     readonly memory?: SerializableObject
   }
 }
+type StoredSpawnRequest = {
+  readonly processId: AnyProcessId
+  readonly identifier: string
+  readonly body: CreepBodyStringRepresentation
+  readonly roomName: RoomName
+  readonly bodyCost: number
+  readonly options?: {
+    readonly codename?: string
+    readonly uniqueCreepName?: CreepName
+    readonly memory?: SerializableObject
+  }
+}
+
 export type V3BridgeSpawnRequestProcessApi = {
   addSpawnRequest<M extends SerializableObject>(body: CreepBody, roomName: RoomName, options?: { codename?: string, uniqueCreepName?: CreepName, memory?: V5CreepMemory<M> }): void
+  addSpawnRequestV2<M extends SerializableObject>(processId: AnyProcessId, identifier: string, body: CreepBody, roomName: RoomName, options?: { codename?: string, uniqueCreepName?: CreepName, memory?: V5CreepMemory<M> }): void
 }
+
+export const v3BridgeSpawnRequestDidSpawnNotification = "v3_spawn_spawned"
+export const v3BridgeSpawnRequestDidFailNotification = "v3_spawn_failed"
+
+export type V3BridgeSpawnRequestDidSpawnNotification = Notification & {
+  readonly eventName: "v3_spawn_spawned"
+  readonly creepName: CreepName
+  readonly processId: AnyProcessId
+  readonly identifier: string
+}
+export type V3BridgeSpawnRequestDidFailNotification = Notification & {
+  readonly eventName: "v3_spawn_failed"
+  readonly processId: AnyProcessId
+  readonly identifier: string
+  readonly errorCode: Exclude<ScreepsReturnCode, OK>
+}
+export type V3BridgeSpawnRequestNotification = V3BridgeSpawnRequestDidSpawnNotification | V3BridgeSpawnRequestDidFailNotification
 
 
 type Dependency = V3BridgeDriverProcessApi
 
-ProcessDecoder.register("V3BridgeSpawnRequestProcess", (processId: V3BridgeSpawnRequestProcessId) => V3BridgeSpawnRequestProcess.decode(processId))
-export type V3BridgeSpawnRequestProcessId = ProcessId<Dependency, ProcessDefaultIdentifier, V3BridgeSpawnRequestProcessApi, EmptySerializable, V3BridgeSpawnRequestProcess>
+type V3BridgeSpawnRequestProcessState = {
+  readonly r: StoredSpawnRequest[]  /// Spawn requests
+}
 
 
-export class V3BridgeSpawnRequestProcess extends Process<Dependency, ProcessDefaultIdentifier, V3BridgeSpawnRequestProcessApi, EmptySerializable, V3BridgeSpawnRequestProcess> {
+ProcessDecoder.register("V3BridgeSpawnRequestProcess", (processId: V3BridgeSpawnRequestProcessId, state: V3BridgeSpawnRequestProcessState) => V3BridgeSpawnRequestProcess.decode(processId, state))
+export type V3BridgeSpawnRequestProcessId = ProcessId<Dependency, ProcessDefaultIdentifier, V3BridgeSpawnRequestProcessApi, V3BridgeSpawnRequestProcessState, V3BridgeSpawnRequestProcess>
+
+
+export class V3BridgeSpawnRequestProcess extends Process<Dependency, ProcessDefaultIdentifier, V3BridgeSpawnRequestProcessApi, V3BridgeSpawnRequestProcessState, V3BridgeSpawnRequestProcess> {
   public readonly identifier = processDefaultIdentifier
   public readonly dependencies: ProcessDependencies = {
     processes: [
@@ -45,20 +82,23 @@ export class V3BridgeSpawnRequestProcess extends Process<Dependency, ProcessDefa
 
   private constructor(
     public readonly processId: V3BridgeSpawnRequestProcessId,
+    private readonly storedSpawnRequests: StoredSpawnRequest[],
   ) {
     super()
   }
 
-  public encode(): EmptySerializable {
-    return {}
+  public encode(): V3BridgeSpawnRequestProcessState {
+    return {
+      r: this.storedSpawnRequests,
+    }
   }
 
-  public static decode(processId: V3BridgeSpawnRequestProcessId): V3BridgeSpawnRequestProcess {
-    return new V3BridgeSpawnRequestProcess(processId)
+  public static decode(processId: V3BridgeSpawnRequestProcessId, state: V3BridgeSpawnRequestProcessState): V3BridgeSpawnRequestProcess {
+    return new V3BridgeSpawnRequestProcess(processId, state.r)
   }
 
   public static create(processId: V3BridgeSpawnRequestProcessId): V3BridgeSpawnRequestProcess {
-    return new V3BridgeSpawnRequestProcess(processId)
+    return new V3BridgeSpawnRequestProcess(processId, [])
   }
 
   public getDependentData(sharedMemory: ReadonlySharedMemory): Dependency | null {
@@ -89,6 +129,17 @@ export class V3BridgeSpawnRequestProcess extends Process<Dependency, ProcessDefa
         })
         // SystemCalls.logger.log(this, `Added spawn request ${body.stringRepresentation} in ${ConsoleUtility.roomLink(roomName)}`)
         // TODO: 内部的にSystemCalls.Loggerを呼び出すOnHeapLogger
+      },
+
+      addSpawnRequestV2: <M extends SerializableObject>(processId: AnyProcessId, identifier: string, body: CreepBody, roomName: RoomName, options?: { codename?: string, uniqueCreepName?: CreepName, memory?: V5CreepMemory<M> }): void => {
+        this.storedSpawnRequests.push({
+          processId,
+          identifier,
+          body: body.stringRepresentation,
+          bodyCost: body.energyCost,
+          roomName,
+          options,
+        })
       },
     }
   }
@@ -130,5 +181,62 @@ export class V3BridgeSpawnRequestProcess extends Process<Dependency, ProcessDefa
 
       SystemCalls.logger.log(this, `${idleSpawn.name} in ${ConsoleUtility.roomLink(idleSpawn.room.name)}: ${result}`)
     })
+
+    const deleteIndices: number[] = []
+    this.storedSpawnRequests.forEach((request, index) => {
+      if (skipRoomNames.includes(request.roomName) === true) {
+        return
+      }
+
+      const spawnInfo = dependency.getIdleSpawnsFor(request.roomName)
+      if (spawnInfo == null) {
+        skipRoomNames.push(request.roomName)
+        return
+      }
+
+      const idleSpawn = spawnInfo.idleSpawns[0]
+      if (idleSpawn == null) {
+        skipRoomNames.push(request.roomName)
+        return
+      }
+
+      if (request.bodyCost > spawnInfo.remainingEnergy) {
+        return
+      }
+
+      const creepName = request.options?.uniqueCreepName != null ? request.options.uniqueCreepName : SystemCalls.uniqueName.generate(request.options?.codename)
+      const options: SpawnOptions = {}
+      if (request.options?.memory != null) {
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        (options.memory as any) = request.options.memory
+      }
+      const result = idleSpawn.spawnCreep(CreepBody.createFromStringRepresentation(request.body).bodyParts, creepName, options)
+      if (result === OK) {
+        skipRoomNames.push(request.roomName)
+        this.sendNotification({
+          eventName: "v3_spawn_spawned",
+          processId: request.processId,
+          identifier: request.identifier,
+          creepName,
+        })
+      } else {
+        this.sendNotification({
+          eventName: "v3_spawn_failed",
+          processId: request.processId,
+          identifier: request.identifier,
+          errorCode: result,
+        })
+      }
+
+      deleteIndices.push(index)
+      SystemCalls.logger.log(this, `${idleSpawn.name} in ${ConsoleUtility.roomLink(idleSpawn.room.name)}: ${result}`)
+    })
+
+    deleteIndices.reverse()
+    deleteIndices.forEach(index => this.storedSpawnRequests.splice(index, 1))
+  }
+
+  private sendNotification(notification: V3BridgeSpawnRequestNotification): void {
+    SystemCalls.notificationManager.send(notification)
   }
 }
