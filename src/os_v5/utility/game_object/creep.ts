ccbe036ac93a2c3896cb7747bf2292547a79023b
Yamazaki Mitsuyoshi <mitsuyoshi.tsukuba@gmail.com>
2024-08-16 09:12:27 +0900

    コメントを削除

--- src/os_v5/utility/game_object/creep.ts
+++ src/os_v5/utility/game_object/creep.ts
@@ -9,7 +9,7 @@ export type ExtendedV5CreepMemory<M extends SerializableObject> = M extends V5Cr
 
 export type V5CreepMemory<M extends SerializableObject> = {
   readonly v: "o5"  // アプリケーションの"v5"がすでに存在するため
-  p: AnyProcessId | null /// リアロケーション可能
+  p: AnyProcessId | null // リアロケーション可能
 } & M
 
 export type V5Creep<M extends SerializableObject> = Omit<Creep, "memory"> & {
